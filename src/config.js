1b7f105b1320ec547d0a35d2176904de7aba6d2f
KagaK2 <KhanhPhan2512@gmail.com>
2019-04-11 11:44:50 +0300

    change to API key to deploy on Heroku

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "shortcut-visitors.firebaseapp.com",
+  databaseURL: "https://shortcut-visitors.firebaseio.com",
+  projectId: "shortcut-visitors",
+  storageBucket: "shortcut-visitors.appspot.com",
+  messagingSenderId: "414093097502"
+};
+
+export default config;
